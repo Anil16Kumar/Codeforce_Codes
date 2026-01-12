5476ef8bb824b305ade0a92d9897c71930824c1d
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-01-12 18:32:25 +0530

    made code changes

--- src/CC/COupleEntry.java
+++ src/CC/COupleEntry.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class COupleEntry {
-    public static <Consume> void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         int t=scanner.nextInt();
 
