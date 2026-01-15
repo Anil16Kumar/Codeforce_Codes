8af4b36771058c2805ffb8c0e7175fbac5e28b75
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-01-16 01:44:16 +0530

    practice code on codechef

--- src/CC/Migration.java
+++ src/CC/Migration.java
@@ -3,19 +3,17 @@ package CC;
 import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
-public class water {
+public class Migration {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
 
-        Function<Integer,Integer> func=num->num*2;
-
+        Function<int[],Integer> func=Arr->(Arr[0]-Arr[1])+Arr[2];
         Consumer<Scanner> perfromTestCases=sc->{
-            System.out.println(func.apply(sc.nextInt()));
+            System.out.println(func.apply(new int[]{sc.nextInt(),sc.nextInt(),sc.nextInt()}));
         };
-
+//        System.out.println(Math.min(1,2));
         IntStream.range(0,scanner.nextInt()).forEach(i->perfromTestCases.accept(scanner));
     }
 }
