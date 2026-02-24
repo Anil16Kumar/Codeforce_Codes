97829387b2529ace4ec65e888cff6300f2764da9
Anil Kumar <anilkumarsingh0160@gmail.com>
2026-02-24 19:58:25 +0530

    code practice

--- src/cf/spellCheck.java
+++ src/cf/spellCheck.java
@@ -1,7 +1,31 @@
 package cf;
 
+import java.util.Scanner;
+
 public class spellCheck {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int t=scanner.nextInt();
+        while (t-->0){
+            int n=scanner.nextInt();
+            String inp=scanner.next();
+            String smp="Timur";
+            int sum=0;
+            for(int i=0;i<smp.length();i++)
+                sum+=smp.charAt(i);
 
+            if(n!=5)
+                System.out.println("NO");
+            else{
+                for(int i=0;i<inp.length();i++)
+                    sum-=inp.charAt(i);
+
+                if(sum==0)
+                    System.out.println("YES");
+                else
+                    System.out.println("NO");
+            }
+
+        }
     }
 }
